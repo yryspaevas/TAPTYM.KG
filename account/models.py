8780b2bc39d6277178805e6c5f5baeda0b735203
Ruslan <ruslan883888@gmail.com>
2023-01-16 16:26:27 +0600

    account+swager+cors+smtp+celery

--- account/models.py
+++ account/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.crypto import get_random_string
+from .tasks import send_activation_code
+from django.core.mail import send_mail
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password, **kwargs):
+        assert email, "Email is required"
+        email = self.normalize_email(email)
+        user:User = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.create_activation_code()
+        user.save(using=self._db)
+        send_activation_code.delay(user.email, user.activation_code)
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        assert email, 'Email is required'
+        kwargs['is_active'] = True
+        kwargs['is_superuser'] = True
+        kwargs['is_staff'] = True
+        email = self.normalize_email(email)
+        user:User = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = None
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='users', null=True)
+    is_active = models.BooleanField(default=False)
+    activation_code = models.CharField(max_length=8, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def create_activation_code(self):
+        self.activation_code = get_random_string(8, 'qwertyuiopasdfghjklzxcvbnm123456789')
+
+   
+    def password_confirm(self):
+        activation_url = f'http://127.0.0.1:8000/account/password_confirm/{self.activation_code}'
+        message = f"""
+        Do you want to change password?
+        Confirm password changes: {activation_url}
+        """
+        send_mail("Please confirm", message, "ruslan883888@gmail.com", [self.email])
+
+    def __str__(self) -> str:
+        return f'{self.username} -> {self.email}'
--- account/views.py
+++ account/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from .serializers import *
+from .models import User
 
-# Create your views here.
+
+class RegisterUserView(APIView):
+    @swagger_auto_schema(request_body=RegisterUserSerializer())
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response("Вы успешно зарегистрировались", status=201)
+
+class ForgotPasswordView(APIView):
+    @swagger_auto_schema(request_body=ForgotSerializer)
+    def post(self, request):
+        data = request.POST
+        serializer = ForgotSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            message = "Please, confirm your email"
+            return Response(message)
+
+class DeleteUserView(APIView):
+    def delete(self, request,email):
+        user = get_object_or_404(User, email=email)
+        print(user)
+        print(request.user)
+        if request.user.is_staff:
+            user.delete()
+            return Response(status=204) 
+        return Response(status=403)
+
+class NewPasswordView(APIView):
+    def get(self, request, activation_code):
+        user = get_object_or_404(User, activation_code=activation_code)
+        new_password = user.generate_activation_code()
+        user.set_password(new_password)
+        user.save()
+        return Response(f"Your new password is {new_password}")
+
+@api_view(['GET'])
+def activate_view(request, activation_code):
+    user = get_object_or_404(User, activation_code=activation_code)
+    user.is_active = True # делаем активным
+    user.activation_code = '' # удаляем активационный код
+    user.save()
+    return Response('Вы успешно активировали аккаунт', status=200)
+
+        
