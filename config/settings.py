26527b1bd2473e48013ae1371c241da11420d8a9
eles <yryspaeva22@gmail.com>
2023-01-16 17:12:00 +0600

    host

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['34.123.240.158','127.0.0.1']
 
 
 # Application definition
