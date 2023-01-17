31cb057c83c52768ed4a961abad91cfe2375eed2
Ruslan <ruslan883888@gmail.com>
2023-01-17 10:58:01 +0600

    .

--- account/tasks.py
+++ account/tasks.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 
 @shared_task
 def send_activation_code(email, activation_code):
-    activation_link = f'http://127.0.0.1:8000/account/activate/{activation_code}'
+    activation_link = f'http://34.123.240.158/account/activate/{activation_code}'
     message = f'Нажми на ссылку, для активации\n{activation_link}'
     send_mail('Activate account', message, 'admin@admin.com', recipient_list=[email])
 
